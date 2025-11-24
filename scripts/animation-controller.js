1642fa4295eaebecd725e87b8859d338f6a4e7ce
Richardbrunei <lianglin46@outlook.com>
2025-11-24 12:06:42 -0600

    Fixed animations

--- scripts/animation-controller.js
+++ scripts/animation-controller.js
@@ -738,9 +738,9 @@ export class AnimationController {
       'B': {
         cycle: ['U', 'R', 'D', 'L'],
         positions: {
-          U: [2, 1, 0],      // Top row
-          L: [0, 3, 6],      // Left column
-          D: [6, 7, 8],      // Bottom row
+          U: [0, 1, 2],      // Top row
+          L: [6, 3, 0],      // Left column
+          D: [8, 7, 6],      // Bottom row
           R: [2, 5, 8]       // Right column
         }
       }
